3d118d0c36cc5da92194004d58844748c94b8e5e
leesoli0122 <leesoli0122@gmail.com>
2026-06-09 09:12:04 +0900

    수정

--- GUIDE-REACT/src/App.jsx
+++ GUIDE-REACT/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       {/* {임시 네비게이션 - 나중에 header 컴포넌트로 분리} */}
-      <nav style={{display: 'flex', gap: '16px', padding: '12px  24px', borderBottom: '1px solid #e5e7eb'}}>
+      <nav style={{ position: 'fixed', display: 'flex', gap: '16px', width: '100%', padding: '12px  24px', borderBottom: '1px solid #e5e7eb', backgroundColor: '#fff', zIndex: 100 }}>
         <Link to="/">홈</Link>
         <Link to="/components">컴포넌트</Link>
       </nav>
